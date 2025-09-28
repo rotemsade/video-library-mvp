8d24e8cc4b9e3bfc2a15e9608ef1b0fd9fa04d8e
Rotem Sade <rotemsade71@gmail.com>
2025-09-28 06:11:03 +0300

    VL-7 : Adding project essentials

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+// frontend/index.js
+// Entry file for Expo: registers your root component (App.tsx).
+
+import { registerRootComponent } from 'expo';
+import App from './App';
+
+// Register the root component with the native runtime
+registerRootComponent(App);
